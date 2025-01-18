ea98438ce88cef5b65630da7cbe90a21ea091e95
Alexis VACHET <alexis.vachet@outlook.fr>
2025-01-18 23:17:37 +0100

    added border effect to SponsorCard

--- app/components/SponsorCard.vue
+++ app/components/SponsorCard.vue
@@ -25,12 +25,12 @@ defineProps({
       :to="link"
       target="_blank"
       rel="noopener noreferrer"
-      class="sm: flex w-24 flex-col items-center gap-5 rounded-2xl outline-2 outline-primary hover:outline sm:w-32 md:w-40 xl:w-60"
+      class="hover:outline-offset-3 flex w-24 flex-col items-center gap-5 rounded-2xl p-2 outline outline-2 outline-slate-700 transition-all hover:outline hover:outline-2 hover:outline-primary hover:duration-300 hover:ease-in-out sm:w-32 md:w-40 xl:w-60"
     >
       <img
         :src="image"
         :alt="alt"
-        class="h-24 w-24 rounded-2xl object-contain object-bottom sm:h-32 sm:w-32 md:h-40 md:w-40 xl:h-44 xl:w-44"
+        class="aspect-square h-24 w-24 rounded-2xl object-contain object-bottom sm:h-32 sm:w-32 md:h-40 md:w-40 xl:h-44 xl:w-44"
       />
       <h2
         class="w-full text-center text-xl font-semibold text-secondary dark:text-white"
