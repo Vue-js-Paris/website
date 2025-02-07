dc78ce82f7c4dd636395516d507f20cb124b8852
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-07 18:43:14 +0100

    fix home_cover position + change text color

--- app/components/HomeSectionHero.vue
+++ app/components/HomeSectionHero.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="h-[110vh] w-full bg-fixed bg-center bg-no-repeat"
+    class="h-[110vh] w-full bg-fixed bg-right bg-no-repeat"
     style="background-image: url(&quot;/images/Paris_cover.webp&quot;)"
   >
     <div class="flex h-[120vh] w-full flex-col gap-28 md:mx-auto md:w-[90%]">
@@ -13,7 +13,7 @@
         <h2 class="text-3xl font-semibold text-white sm:text-5xl">
           Découvrir, partager et connecter.
         </h2>
-        <p>
+        <p class="text-slate-300">
           Rejoignez-nous pour notre prochain meetup et découvrez des talks
           passionnants et des moments de partage avec la communauté Vue.js
           Paris.
