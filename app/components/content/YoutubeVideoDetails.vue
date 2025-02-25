750cdfa834d432960849f732505237804097f5b9
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-25 13:41:37 +0100

    modif takls details

--- app/components/content/YoutubeVideoDetails.vue
+++ app/components/content/YoutubeVideoDetails.vue
@@ -7,21 +7,23 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="w-full pt-10 md:px-10">
     <article
-      class="flex w-full flex-col px-5 md:mx-auto md:w-[90%] md:px-0 xl:w-1/2"
+      class="background w-full border-2 border-bordercolor p-5 md:p-10 xl:p-20"
     >
-      <h1 class="mb-10 mt-10 text-center text-2xl text-white md:text-3xl">
-        {{ videoTitle }}
-      </h1>
-      <iframe
-        class="h-[300px] w-full rounded-2xl sm:h-[350px] md:h-[550px]"
-        :src="`https://www.youtube.com/embed/${videoId}`"
-        frameborder="0"
-        allowfullscreen
-      />
-      <div class="prose-base pt-10 text-white">
-        <slot />
+      <div class="mx-auto flex w-full flex-col xl:w-2/3">
+        <h1 class="mb-10 text-center text-2xl text-white md:text-3xl">
+          {{ videoTitle }}
+        </h1>
+        <iframe
+          class="h-[300px] w-full rounded-2xl sm:h-[350px] md:h-[550px]"
+          :src="`https://www.youtube.com/embed/${videoId}`"
+          frameborder="0"
+          allowfullscreen
+        />
+        <div class="prose-base pt-10 text-white">
+          <slot />
+        </div>
       </div>
     </article>
   </div>
